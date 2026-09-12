22cca135a118c6372468118053fa1d89838b9c38
jhrhq <jhr.code@proton.me>
2026-09-12 23:58:29 +0600

    fetch users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
+import { Todo } from './todo/todo';
+import { UserList } from './user-list/user-list';
 
 export const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    loadComponent: () => import('./app').then((m) => m.App),
-  },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   loadComponent: () => import('./app').then((m) => m.App),
+  // },
   {
     path: 'todos',
-    loadComponent: () => import('./todo/todo').then((m) => m.Todo),
+    loadComponent: () => Todo,
+  },
+  {
+    path: 'users',
+    loadComponent: () => UserList,
   },
 ];
---
+++ src/app/user-list/user-list.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject, signal } from '@angular/core';
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+@Component({
+  imports: [],
+  selector: 'app-user-list',
+  styleUrl: './user-list.css',
+  templateUrl: './user-list.html',
+})
+export class UserList implements OnInit {
+  private http = inject(HttpClient);
+  users = signal<any[]>([]);
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('https://jsonplaceholder.typicode.com/users')
+      .subscribe((data) => this.users.set(data));
+  }
+}
