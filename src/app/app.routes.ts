371e297b65a6e7721c062fed3cdb6389a45d9c40
jhrhq <jhr.code@proton.me>
2026-09-13 13:38:05 +0600

    button, event inputs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { Todo } from './todo/todo';
 import { UserList } from './user-list/user-list';
+import { App } from './app';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   pathMatch: 'full',
-  //   loadComponent: () => import('./app').then((m) => m.App),
-  // },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: App,
+  },
   {
     path: 'todos',
     loadComponent: () => Todo,
--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,22 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly username = signal('Jhr');
+  count = 0;
+  enable = false;
+
+  counter(action: string) {
+    if (action === 'minus') {
+      this.count > 0 && this.count--;
+    } else {
+      this.count++;
+    }
+  }
+
+  handleEvent(event: string) {
+    console.log(event);
+  }
+
+  toggleEnable() {
+    this.enable = !this.enable;
+  }
 }
