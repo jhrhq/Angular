a34ac93407c5c16da005b15440ad8bb5a8dfc4f5
jhrhq <jhr.code@proton.me>
2026-09-22 21:46:22 +0600

    navigate user back to home page on click

--- src/app/pages/page-not-found/page-not-found.ts
+++ src/app/pages/page-not-found/page-not-found.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   imports: [],
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './page-not-found.css',
   templateUrl: './page-not-found.html',
 })
-export class PageNotFound {}
+export class PageNotFound {
+  private router = inject(Router);
+
+  goToHome() {
+    this.router.navigate(['/']);
+  }
+}
