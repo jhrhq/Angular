f46e35df48603f2297bd2f02e1ff40c7a4e755db
jhrhq <jhr.code@proton.me>
2026-09-22 21:14:00 +0600

    not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Profile } from './pages/profile/profile';
 import { App } from './app';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
 export const routes: Routes = [
   { path: '', component: App },
   { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: 'profile', component: Profile },
+  { path: '**', component: PageNotFound },
+  // { path: '**', redirectTo: '' },
 ];
