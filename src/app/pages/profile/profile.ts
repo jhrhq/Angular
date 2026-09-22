d748ee5875c1a99d98a14c5aebcb0a66276ad857
jhrhq <jhr.code@proton.me>
2026-09-22 22:01:52 +0600

    adding query params

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   imports: [],
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './profile.css',
   templateUrl: './profile.html',
 })
-export class Profile {}
+export class Profile {
+  private route = inject(ActivatedRoute);
+  id: string | null = null;
+  name: string | null = null;
+  age: string | null = null;
+
+  ngOnInit() {
+    this.route.queryParamMap.subscribe((params) => {
+      this.id = params.get('id');
+      this.name = params.get('name');
+      this.age = params.get('age');
+    });
+  }
+}
